689f1f93aaba33d50310225df39a22df2a70d6d7
Wwright91 <wisdomwright-martin@pursuit.org>
2024-12-04 00:45:35 -0500

    /api/complaints/topComplaints/
    Created view to get the top 3 complaint types from the user's district

--- challenge/complaint_app/views.py
+++ challenge/complaint_app/views.py
@@ -3,6 +3,7 @@ from .models import UserProfile, Complaint
 from .serializers import UserSerializer, UserProfileSerializer, ComplaintSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Count
 # Create your views here.
 
 class ComplaintViewSet(viewsets.ModelViewSet):
@@ -43,4 +44,8 @@ class TopComplaintTypeViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
   def list(self, request):
     # Get the top 3 complaint types from the user's district
-    return Response()
+    user_profile = UserProfile.objects.get(user=request.user)
+    user_district = user_profile.district
+    top_3_complaint_types = Complaint.objects.filter(account=f"NYCC{user_district}").values('complaint_type').annotate(count=Count('complaint_type')).order_by('-count')[:3]
+            
+    return Response(top_3_complaint_types)
